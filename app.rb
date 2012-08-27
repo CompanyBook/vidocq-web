d90074b986fb16e372996e2dd68522b1dd767c2f
Alexander Mossin <alexander@companybook.no>
2012-08-27 09:09:51 +0200

    Add basic index.html

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require 'vidocq'
 require 'json'
 
 get '/' do
+  send_file File.join(settings.public_folder, 'index.html')
+end
+
+get '/index.json' do
   vidocq = Vidocq.new
   content_type :json
   return vidocq.services.to_json
