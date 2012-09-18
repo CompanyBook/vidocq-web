090a70c18fddb74839475520ceefc9f819c094d6
Alexander Mossin <alexander@companybook.no>
2012-09-18 10:31:39 +0200

    Vidcoq update

--- app.rb
+++ app.rb
@@ -1,13 +1,11 @@
 require 'sinatra'
 require 'vidocq'
-require 'json'
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
 
 get '/index.json' do
-  vidocq = Vidocq.new('namenode.companybook.no:2181,sec-namenode.companybook.no:2181,jobtracker.companybook.no:2181')
   content_type :json
-  return vidocq.services.to_json
+  return Vidocq.services('namenode.companybook.no:2181,sec-namenode.companybook.no:2181,jobtracker.companybook.no:2181').to_json
 end
